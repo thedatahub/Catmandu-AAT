7a1ad89b45d34f52ea787088b631594b95df69b3
Pieter De Praetere <pieter@packed.be>
2017-01-05 15:58:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Catmandu::Store::AAT - Blah blah blah
+
+# SYNOPSIS
+
+    use Catmandu::Store::AAT;
+
+# DESCRIPTION
+
+Catmandu::Store::AAT is
+
+# AUTHOR
+
+Pieter De Praetere <pieter.de.praetere@helptux.be>
+
+# COPYRIGHT
+
+Copyright 2017- Pieter De Praetere
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Catmandu/Store/AAT.pm
@@ -0,0 +1,39 @@
+package Catmandu::Store::AAT;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catmandu::Store::AAT - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Catmandu::Store::AAT;
+
+=head1 DESCRIPTION
+
+Catmandu::Store::AAT is
+
+=head1 AUTHOR
+
+Pieter De Praetere E<lt>pieter.de.praetere@helptux.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Pieter De Praetere
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
